3fa0afd9c111566ad68e3f0812ba3e32c78d593e
Mehak Arora <Tailwind>
2026-01-20 15:50:37 +0530

    Fix Vercel deployment

--- right-click-institute/src/postcss.config.js
+++ right-click-institute/src/postcss.config.js
@@ -1,6 +1,7 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: "./",
+  plugins: [react()],
+})
