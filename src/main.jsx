7d9c7e7c4580e8458c58df18664375a1598d55d5
Mehak Arora <Tailwind>
2026-02-02 11:46:55 +0530

    Add _redirects

--- src/main.jsx
+++ src/main.jsx
@@ -7,3 +7,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
